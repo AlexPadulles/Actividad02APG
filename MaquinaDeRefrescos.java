ca087f49ee62073097dfc3f3846f5e757f227174
AlexPadulles <87446258+AlexPadulles@users.noreply.github.com>
2022-02-22 21:30:37 +0100

    Update and rename Actividad02APG.java to MaquinaDeRefrescos.java

--- MaquinaDeRefrescos.java
+++ MaquinaDeRefrescos.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Actividad02APG {
+public class MaquinaDeRefrescos {
 
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) throws IOException {
